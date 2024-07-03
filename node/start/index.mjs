72213a9d469bf5ede7da37ff637dc476aa06db76
Christo Kruger <christo.kruger@bbd>
2024-07-03 18:40:03 +0200

    start

---
+++ node/start/index.mjs
@@ -0,0 +1,43 @@
+import pkg from 'pg';
+const { Client } = pkg;
+import fetch from 'node-fetch';
+
+export async function handler (event, context) {
+  
+  const url = `https://api.mers.projects.bbdgrad.com/api/taxpayer/business/register`;
+    const body = {
+      "businessName": "Free Money Inc. (home lons)",
+    }
+
+    const result = await fetch(url, {
+      method: 'POST',
+      body: body
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Network response was not ok: ${response.json()}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        return data;
+      })
+      .catch((error) => {
+        console.error('There has been a problem with your fetch operation:', error);
+      });
+
+      console.log(result);
+
+  const client = new Client({
+    host: 'home-loans-service.c9jnw8blzgak.eu-west-1.rds.amazonaws.com',
+    user: 'admin_usr',
+    password: '<[I1cgqA:|n5pa~cHDAT+lIUayDW',
+    database: 'homeloan',
+    port: '5432',
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  });
+
+}
